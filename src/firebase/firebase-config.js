c83bba80020894164b3bde9f488dd4cfb1672b7e
Fabian Higuita <fabianes1996@gmail.com>
2022-03-01 13:13:58 -0500

    Add initial firebase configuration

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAETcXS4LzwN5JKxAIyNAIw58dzEII4xR8",
+  authDomain: "react-movies-app-9494f.firebaseapp.com",
+  projectId: "react-movies-app-9494f",
+  storageBucket: "react-movies-app-9494f.appspot.com",
+  messagingSenderId: "317920290312",
+  appId: "1:317920290312:web:98abaea2d287b0098613bd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
