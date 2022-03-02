1efea071ba303871dc8c54d73762f5b2c9ecd8c0
Fabian Higuita <fabianes1996@gmail.com>
2022-03-02 16:01:40 -0500

    Update registerUser function to add username

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth, createUserWithEmailAndPassword} from 'firebase/auth'
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,5 +20,6 @@ const auth = getAuth()
 export {
   app,
   auth,
-  createUserWithEmailAndPassword
+  createUserWithEmailAndPassword,
+  updateProfile
 }
