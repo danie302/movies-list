755e4adb89498d85c9d71194e4847fd8e5a4aeb1
Fabian Higuita <fabianes1996@gmail.com>
2022-03-02 17:53:08 -0500

    Update index & add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login/Login';
 import Signup from './components/Signup/Signup';
 
 function App() {
   return (
-    <>
-      <Signup />
-    </>
+    <main>
+      <Routes>
+        <Route path='/' element={<Navigate replace to='/login' />}/>
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
+      <Login />
+    </main>
   );
 }
 
