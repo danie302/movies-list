177eab0005c68f1f5db67f2b394e9b45948ff8c7
Fabian Higuita <fabianes1996@gmail.com>
2022-03-03 11:12:05 -0500

    Create pages folder & update routes file

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,16 +1,16 @@
 
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
-import Login from '../components/Login/Login'
-import Signup from '../components/Signup/Signup'
+import LoginPage from '../pages/LoginPage'
+import SignupPage from '../pages/SignupPage'
 
 const AppRoutes = () => {
   return (
     <main>
           <Routes>
             <Route path='/' element={<Navigate replace to='/login' />}/>
-            <Route path='/login' element={<Login />} />
-            <Route path='/signup' element={<Signup />} />
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/signup' element={<SignupPage />} />
           </Routes>
     </main>    
   )
