5b30e42d6c7a35654226cbb7bd481ebc4d51a162
Fabian Higuita <fabianes1996@gmail.com>
2022-03-04 15:51:23 -0500

    Fetch data from API & render it in HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,29 @@
-import React, { useContext } from 'react'
+import React, { useCallback, useContext, useEffect, useState } from 'react'
+import { getPopularMovies } from '../services/moviesService';
 import { AuthContext } from '../store/auth-context'
 
 const HomePage = () => {
+
+  const [popularMovies, setPopularMovies] = useState({})
   const authCtx = useContext(AuthContext);
-  const user = authCtx.user?.uid ? authCtx.user : JSON.parse(authCtx.user)
+  const user = authCtx.user?.uid ? authCtx.user : JSON.parse(authCtx.user);
+  console.log('renderizando');
+  const movies = popularMovies?.results;
+  useEffect(() => {
+    const fetchMovies = async()=>{
+      const data = await getPopularMovies();
+      setPopularMovies(data);
+    }
+    fetchMovies()
+      .catch(error => error)
+  }, [])
+  
+  console.log(popularMovies);
   return (
     <div>
-      <p>Welcome {user.name}</p>
+      {movies && movies.map((movie)=>{
+        return <p key={movie.id}>{movie.title}</p>
+      })}
     </div>
   )
 }
