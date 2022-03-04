fc71d4661a13f35f43eabb914d1268a439271a99
Fabian Higuita <fabianes1996@gmail.com>
2022-03-04 14:17:21 -0500

    Display username in HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../store/auth-context'
 
 const HomePage = () => {
+  const authCtx = useContext(AuthContext);
+  const user = JSON.parse(authCtx.user);
   return (
-    <div>HomePage</div>
+    <div>
+      <p>Welcome {user.name}</p>
+    </div>
   )
 }
 
