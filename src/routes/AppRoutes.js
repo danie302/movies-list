6192e3d2e0f358564d113f36eded438a9af30f06
Fabian Higuita <fabianes1996@gmail.com>
2022-03-04 14:13:48 -0500

    Add HomePage route

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,19 +1,37 @@
-
-import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import LoginPage from '../pages/LoginPage'
-import SignupPage from '../pages/SignupPage'
+import React, { useContext } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
+import SignupPage from "../pages/SignupPage";
+import { AuthContext } from "../store/auth-context";
 
 const AppRoutes = () => {
+  const authCtx = useContext(AuthContext);
+  console.log(authCtx);
   return (
     <main>
-          <Routes>
-            <Route path='/' element={<Navigate replace to='/login' />}/>
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/signup' element={<SignupPage />} />
-          </Routes>
-    </main>    
-  )
-}
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            !authCtx.user ? <LoginPage /> : <Navigate replace to="/home" />
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            !authCtx.user ? <SignupPage /> : <Navigate replace to="/home" />
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            authCtx.user ? <HomePage /> : <Navigate replace to="/login" />
+          }
+        />
+      </Routes>
+    </main>
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
