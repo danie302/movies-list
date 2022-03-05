a00f59e3fe4327c1f90d061131bc2ab07e28c40b
Fabian Higuita <fabianes1996@gmail.com>
2022-03-04 19:11:37 -0500

    Update HomePage to show carrousel

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,31 +1,9 @@
-import React, { useCallback, useContext, useEffect, useState } from 'react'
-import { getPopularMovies } from '../services/moviesService';
-import { AuthContext } from '../store/auth-context'
+import Carrousel from "../components/Carrousel/Carrousel";
 
-const HomePage = () => {
-
-  const [popularMovies, setPopularMovies] = useState({})
-  const authCtx = useContext(AuthContext);
-  const user = authCtx.user?.uid ? authCtx.user : JSON.parse(authCtx.user);
-  console.log('renderizando');
-  const movies = popularMovies?.results;
-  useEffect(() => {
-    const fetchMovies = async()=>{
-      const data = await getPopularMovies();
-      setPopularMovies(data);
-    }
-    fetchMovies()
-      .catch(error => error)
-  }, [])
-  
-  console.log(popularMovies);
+const HomePage = () => {  
   return (
-    <div>
-      {movies && movies.map((movie)=>{
-        return <p key={movie.id}>{movie.title}</p>
-      })}
-    </div>
-  )
-}
+    <Carrousel />
+  );
+};
 
-export default HomePage
+export default HomePage;
