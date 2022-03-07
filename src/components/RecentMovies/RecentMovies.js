7c6ee015e02239fe5700cae5c34c2b5d1551dff1
Fabian Higuita <fabianes1996@gmail.com>
2022-03-07 16:38:47 -0500

    Add infinite scroll

--- src/components/RecentMovies/RecentMovies.js
+++ src/components/RecentMovies/RecentMovies.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import './RecentMovies.css';
+import React, { useState } from "react";
+import { LinearProgress } from "@mui/material";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { getRecentMovies } from "../../services/moviesService";
+import "./RecentMovies.css";
 
 const RecentMovies = (props) => {
-   return (
+  const [hasMore, setHasMore] = useState(true);
+  const [movies, setMovies] = useState(props.recent);
+  const [page, setPage] = useState(2);
+
+  const fetchMovies = async () => {
+    const { results } = await getRecentMovies(page);
+    setMovies([...movies, ...results]);
+    if (results.length === 0 || results.length < 20) {
+      setHasMore(false);
+    }
+    setPage(page + 1);
+  };
+
+  return (
     <section id="recent_movies_section">
-      <h2 className='section_title'>Recent Movies</h2>
-      <div className="container_recent">
-        {        
-          props.recent.map((movie)=>(
-            <div key={movie.id} >
-            <img src={props.conf.images.base_url +
-              props.conf.images.poster_sizes[4] + movie.poster_path} alt="" />
+      <h2 className="section_title">Recent Movies</h2>
+
+      <InfiniteScroll
+        dataLength={movies.length}
+        next={fetchMovies}
+        hasMore={hasMore}
+        loader={<LinearProgress className="linear" />}
+        endMessage={
+          <p>There are no more recent movies</p>
+        }
+      >
+        <div className="container_recent">
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img
+                src={
+                  props.conf.images.base_url +
+                  props.conf.images.poster_sizes[4] +
+                  (movie.poster_path !== null
+                    ? movie.poster_path
+                    : movie.backdrop_path)
+                }
+                alt=""
+              />
               <p>{movie.title}</p>
             </div>
-            
-          ))
-        }
-        
-      </div>
-      
+          ))}
+        </div>
+      </InfiniteScroll>
     </section>
-  )
-}
+  );
+};
 
-export default RecentMovies
+export default RecentMovies;
