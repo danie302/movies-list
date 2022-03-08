dcd658d6311ca69e6e9aa7b9d62cc8c97e248959
Fabian Higuita <fabianes1996@gmail.com>
2022-03-08 18:03:14 -0500

    Add cast-carrousel to MovieDetail component

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import Carrousel from '../Carrousel/Carrousel';
 
 const MovieDetail = (props) => {
+  console.log(props.images);
+  console.log(props.cast);
+  const castArray = props.cast.cast
   return (
     <div>
-      <img src={props.imgPath} alt={props.movie.title} />
+      <img src={props.images.base_url +
+            props.images.poster_sizes[4] +
+            props.movie.poster_path} alt={props.movie.title} />
       <h2>{props.movie.title}</h2>
       <p>{props.movie.overview}</p>
+      {
+        <Carrousel items={castArray} conf={props.images} path={"profile_path"} />        
+      }
     </div>
   )
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,41 +1,50 @@
 import React, { useEffect, useState } from "react";
 import Carrousel from "../components/Carrousel/Carrousel";
 import RecentMovies from "../components/RecentMovies/RecentMovies";
-import { getConfiguration, getPopularMovies, getRecentMovies } from "../services/moviesService";
+import {
+  getConfiguration,
+  getPopularMovies,
+  getRecentMovies,
+} from "../services/moviesService";
 
-
-const HomePage = () => {   
-  
+const HomePage = () => {
   const [moviesAndConf, setMoviesAndConf] = useState({
     popular: {},
-    recent:{},
-    conf: {}, 
+    recent: {},
+    conf: {},
   });
   useEffect(() => {
-    const fetchMovies = async () => {      
+    const fetchMovies = async () => {
       const popular = await getPopularMovies();
       const recent = await getRecentMovies();
       const conf = await getConfiguration();
       const data = {
         popular,
         recent,
-        conf
-      }
+        conf,
+      };
       setMoviesAndConf(data);
     };
     fetchMovies().catch((error) => error);
   }, []);
 
   return (
-      <React.Fragment>
-        {
-          moviesAndConf.popular.results && <Carrousel items={moviesAndConf.popular.results} conf={moviesAndConf.conf} />
-        }
-        {
-          moviesAndConf.recent.results && <RecentMovies recent={moviesAndConf.recent.results} conf={moviesAndConf.conf} />
-        }
-
-      </React.Fragment>
+    <React.Fragment>
+      <h2>Movies List</h2>
+      {moviesAndConf.popular.results && (
+        <Carrousel
+          items={moviesAndConf.popular.results}
+          conf={moviesAndConf.conf.images}
+          path="backdrop_path"
+        />
+      )}
+      {moviesAndConf.recent.results && (
+        <RecentMovies
+          recent={moviesAndConf.recent.results}
+          conf={moviesAndConf.conf}
+        />
+      )}
+    </React.Fragment>
   );
 };
 
