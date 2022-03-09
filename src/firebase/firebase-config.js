883d9eaa0dba609ac63f9d3ac2b98ce319fe0961
Fabian Higuita <fabianes1996@gmail.com>
2022-03-09 18:12:19 -0500

    Add logout function

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth, createUserWithEmailAndPassword, updateProfile, signInWithEmailAndPassword} from 'firebase/auth'
+import {getAuth, createUserWithEmailAndPassword, updateProfile, signInWithEmailAndPassword, signOut} from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,5 +22,6 @@ export {
   auth,
   createUserWithEmailAndPassword,
   updateProfile, 
-  signInWithEmailAndPassword
+  signInWithEmailAndPassword,
+  signOut
 }
