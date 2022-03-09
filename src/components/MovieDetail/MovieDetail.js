64266eaf2f457920ba6af3ad81710bab696dfb08
Fabian Higuita <fabianes1996@gmail.com>
2022-03-08 20:35:40 -0500

    Update movieDetail component

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -1,22 +1,37 @@
-import React from 'react'
-import Carrousel from '../Carrousel/Carrousel';
-
+import React from "react";
+import Carrousel from "../Carrousel/Carrousel";
+import './MovieDetail.css'
 const MovieDetail = (props) => {
-  console.log(props.images);
-  console.log(props.cast);
-  const castArray = props.cast.cast
+  
+  const castArray = props.cast.cast;
   return (
-    <div>
-      <img src={props.images.base_url +
+    <div className="container-details">
+      <div className="container-details-img">
+        <img
+          src={
+            props.images.base_url +
             props.images.poster_sizes[4] +
-            props.movie.poster_path} alt={props.movie.title} />
-      <h2>{props.movie.title}</h2>
-      <p>{props.movie.overview}</p>
-      {
-        <Carrousel items={castArray} conf={props.images} path={"profile_path"} />        
-      }
+            props.movie.poster_path
+          }
+          alt={props.movie.title}
+        />
+      </div>
+      <div className="container-movie-info">
+        <h2>{props.movie.title}</h2>
+        <p>{props.movie.overview}</p>
+        {
+          <Carrousel
+            items={castArray}
+            conf={props.images}
+            path={"profile_path"}
+            effect={""}
+            autoplay={false}
+            className={'swiper-slide-cast'}
+          />
+        }
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MovieDetail
+export default MovieDetail;
