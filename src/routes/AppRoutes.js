7bdb740b2c9ffec9ff6095a3e790fe0a7664f256
Fabian Higuita <fabianes1996@gmail.com>
2022-03-10 10:06:02 -0500

    Add route protection

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -8,33 +8,50 @@ import { AuthContext } from "../store/auth-context";
 
 const AppRoutes = () => {
   const authCtx = useContext(AuthContext);
-  
   return (
     <main>
       <Routes>
-        <Route path="/" element={<Navigate replace to= '/login' />}/>
+        <Route path="/" element={<Navigate replace to="/login" />} />
         <Route
           path="/login"
           element={
-            !authCtx.user ? <LoginPage /> : <Navigate replace to="/home" />
+            !authCtx.user?.isLogged ? (
+              <LoginPage />
+            ) : (
+              <Navigate replace to="/home" />
+            )
           }
         />
         <Route
           path="/signup"
           element={
-            !authCtx.user ? <SignupPage /> : <Navigate replace to="/home" />
+            !authCtx.user?.isLogged ? (
+              <SignupPage />
+            ) : (
+              <Navigate replace to="/home" />
+            )
           }
         />
         <Route
           path="/home"
           element={
-            authCtx.user ? <HomePage /> : <Navigate replace to="/login" />
+            authCtx.user?.isLogged ? (
+              <HomePage />
+            ) : (
+              <Navigate replace to="/login" />
+            )
           }
         />
         <Route
           path="/movie/:id"
-          element={<MovieDetailPage />}
-         />
+          element={
+            authCtx.user?.isLogged ? (
+              <MovieDetailPage />
+            ) : (
+              <Navigate replace to="/login" />
+            )
+          }
+        />
       </Routes>
     </main>
   );
