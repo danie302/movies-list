b90f15fc4aa5ab76af8e12757cdd3d39aeab6840
Fabian Higuita <fabianes1996@gmail.com>
2022-03-11 17:07:33 -0500

    Add search component to home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,18 +1,35 @@
 import React, { useEffect, useState } from "react";
 import Carrousel from "../components/Carrousel/Carrousel";
-import RecentMovies from "../components/RecentMovies/RecentMovies";
+import Catalog from "../components/Catalog/Catalog";
+import Search from "../components/Search/Search";
 import {
   getConfiguration,
   getPopularMovies,
   getRecentMovies,
+  searchMovie,
 } from "../services/moviesService";
 
 const HomePage = () => {
+  const [title, setTitle] = useState('Recent movies');  
   const [moviesAndConf, setMoviesAndConf] = useState({
     popular: {},
     recent: {},
     conf: {},
-  });
+  });  
+  const searchHandler = async(query)=>{   
+    const {results} = await searchMovie(query)
+    setTitle(`Results for ${query}`)
+    setMoviesAndConf({
+      ...moviesAndConf,
+      moviesSearch: {
+        results,
+        query
+      },
+    })
+  }
+  const recentHandler = async()=>{
+    return await getRecentMovies();
+  }
   useEffect(() => {
     const fetchMovies = async () => {
       const popular = await getPopularMovies();
@@ -44,10 +61,14 @@ const HomePage = () => {
           classNameSwiper={'swiper-popular'}
           />
       )}
-      {moviesAndConf.recent.results && (
-        <RecentMovies
+      <Search onSearch={searchHandler}  />
+
+      {moviesAndConf.recent.results && (        
+        <Catalog 
           recent={moviesAndConf.recent.results}
+          search={moviesAndConf?.moviesSearch}
           conf={moviesAndConf.conf}
+          title={title}
         />
       )}
     </React.Fragment>
