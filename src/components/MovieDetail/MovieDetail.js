058d95b8e5fdda2316f35cc4f93b26558040b482
Fabian Higuita <fabianes1996@gmail.com>
2022-03-11 17:32:10 -0500

    Add classname in MovieDetail

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Carrousel from "../Carrousel/Carrousel";
 import "./MovieDetail.css";
+import EventIcon from "@mui/icons-material/Event";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import { yellow } from "@mui/material/colors";
 const MovieDetail = (props) => {
   const { images, movie } = props;
   const { genres } = movie;
@@ -12,17 +15,30 @@ const MovieDetail = (props) => {
           src={images.base_url + images.poster_sizes[4] + movie.poster_path}
           alt={movie.title}
         />
-        <span>{movie.tagline}</span>
-        <span>{movie.vote_average}</span>
+        <span>{movie?.tagline}</span>
+        <div className="container-details-poster-icons">
+          <div className="star">
+            <StarBorderIcon sx={{ color: yellow[500] }} fontSize="small" />
+            <span>{movie?.vote_average}</span>
+          </div>
+          <div className="date">
+            <EventIcon fontSize="small" />
+            <span>{movie?.release_date}</span>
+          </div>
+        </div>
       </div>
       <div className="container-movie-info">
-        <h2>{movie.title}</h2>
-        {genres.map((genre, i) => (
-          <span className="genres-item" key={i}>
-            {genre.name}
-          </span>
-        ))}
-        <p>{movie.overview}</p>
+          <div className="container-movie-info__header">
+            <h2>{movie.title}</h2>
+            <div className="div-genres">
+              {genres.map((genre, i) => (
+                <span className="genres-item" key={i}>
+                  {genre.name}
+                </span>
+              ))}
+            </div>
+            <p>{movie.overview}</p>
+          </div>
         {
           <Carrousel
             items={castArray}
